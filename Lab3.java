f5215eb5499c553cf3e4bb15c693ade28079fe70
Kevin Shilla <104956094+KevSwags@users.noreply.github.com>
2022-10-06 23:37:01 -0400

    Add files via upload

---
+++ Lab3.java
@@ -0,0 +1,26 @@
+
+
+public class Lab3 {
+
+    public static float converter(float fahrenheit) {
+        float convert = 5f/9f * (fahrenheit - 32f);
+        return convert;
+    }
+    public static void main(String[] args) {
+        float fahrenheit;
+        float centigrade;
+
+        fahrenheit = 98.6f;
+        centigrade = 5f/9f * (fahrenheit - 32f);
+        System.out.println("The fahrenheit is: " + fahrenheit + " the centigrade is: " + centigrade);
+
+        System.out.println("FOR LOOP: ");
+        
+
+        for(fahrenheit=0f; fahrenheit <= 40f; fahrenheit+=5f) {
+            centigrade = converter(fahrenheit);
+            System.out.println("Fahrenheit: " + fahrenheit + " centigrade: " + centigrade);
+        }
+
+    }
+}
